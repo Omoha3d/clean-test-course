0edc9cd243b8cbe719b4f90c52b040c84d2e9a87
Osman Mohamednur <m.osman.alc@gmail.com>
2026-09-11 22:13:18 -0400

    Updated the acceptance test in lesson-6 to check for the presence of menu items when selecting different categories. The test now verifies that "Cheese Burger" and "Fajita Tacos" are present when "Handhelds" is selected, and that "Tater Tots" and "Buffalo Wings" are present while ensuring that "Cheese Burger" and "Fajita Tacos" are not present when "Appeteasers" is selected.

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -4,8 +4,22 @@ describe('Shows Appeteasers', () => {
   });
   it('selects Handhelds', () => {
     //TODO: Add a test to check Handhelds
+     cy.contains('Handhelds').click();
+      //Check if Cheese Burger is there.
+     cy.contains('Cheese Burger');
+     //Check if Fajita Tacos is there
+     cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+    //Navigate to Appeteasers.  This makes sure that the link actually exists.
+      cy.contains('Appeteasers').click();
+      //Check if Tater Tots is there.
+      cy.contains('Tater Tots');
+      //Check if Buffalo Wings is there.
+      cy.contains('Buffalo Wings');
+      //Check that Cheese Burger is not there.
+      cy.contains('cheese Burger').should('not.exist');
+      //Check that Fajita Tacos is not there.
+      cy.contains('Fajita Tacos').should('not.exist');
   });
-});
+  });
